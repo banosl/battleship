762610c2dc9bc197b0f14f7d20a1fafe2b24e78f
Ryan Canton <ryancanton999@gmail.com>
2022-10-17 17:57:13 -0600

    Feat: Add definition to render method for cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,11 @@
 class Cell
 
+  attr_reader :coordinate, :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @hit = false
   end
 
   def place_ship(ship)
@@ -14,6 +17,34 @@ class Cell
   end
 
   def fire_upon
-    @ship.hit
+    if !self.empty?
+      @ship.hit
+    end
+    @hit = true
   end
+
+  def fired_upon?
+    @hit
+  end
+
+  def render(show = nil)
+    if !self.fired_upon?
+      if show && !self.empty?
+        return "S"
+      end
+
+      return "."
+
+    elsif self.fired_upon? && self.empty?
+      return "M"
+
+    elsif self.fired_upon? && @ship.sunk?
+      return "X"
+
+    else
+      return "H"
+    end
+  end
+
+
 end
