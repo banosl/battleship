86a7662f955abe2a091a4ccac683da661b9c21c4
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-17 18:52:50 -0600

    Wrote test 1 for ship_spec.rb

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,4 +2,13 @@ require './lib/ship'
 
 RSpec.describe Ship do
 
+#Test 1
+    it 'cruiser exists' do
+
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser).to be_instance_of(Ship)
+
+    end
+
 end
