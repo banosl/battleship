82c90af8b78282f8ae12c32849c949c79e0b84dc
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-18 19:44:42 -0600

    Test: Wrote test 7

--- spec/iteration2_spec.rb
+++ spec/iteration2_spec.rb
@@ -119,4 +119,25 @@ RSpec.describe Board do
         expect(board.valid_placement?(submarine,["A2","A3", "A4"])).to be false
 
     end
+
+#Test 7
+    it 'coordinates for each ship are consecutive' do
+
+        board = Board.new
+
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+        board.valid_placement?(submarine, ["A1", "C1"])
+        board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+        board.valid_placement?(submarine, ["C1", "B1"])
+
+        expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+        expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+        expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+        expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+
+    end
+
 end
