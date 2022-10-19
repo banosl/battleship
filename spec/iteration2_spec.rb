d79ce77e87cc7c74d45478d6712d0dc5e0564d9e
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-18 20:20:09 -0600

    Test: wrote test 10

--- spec/iteration2_spec.rb
+++ spec/iteration2_spec.rb
@@ -172,4 +172,25 @@ RSpec.describe Board do
 
     end
 
+#Placing Ships
+
+#Test 10
+    it 'Ships are placed on the board and cells exist' do
+
+        board = Board.new
+
+        cruiser = Ship.new("Cruiser", 3)
+
+        board.place(cruiser, ["A1", "A2", "A3"])
+
+        cell_1 = board.cells["A1"]
+        cell_2 = board.cells["A2"]
+        cell_3 = board.cells["A3"]
+
+        expect(cell_1).to be_instance_of(Cell)
+        expect(cell_2).to be_instance_of(Cell)
+        expect(cell_3).to be_instance_of(Cell)
+
+    end
+
 end
