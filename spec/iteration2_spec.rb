696c328ab40871f4150bce5b286580814f9c9b82
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-18 19:08:10 -0600

    Test: wrote test 1

--- spec/iteration2_spec.rb
+++ spec/iteration2_spec.rb
@@ -2,7 +2,16 @@ require './lib/board'
 
 RSpec.describe Board do
 
+#The Cells
 
+#Test 1
+    it 'board exists' do
+
+        board = Board.new
+
+        expect(board)to be_instance_of(Board)
+
+    end
 
 
 
