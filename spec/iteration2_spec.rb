5d1b52505963afd122674b9f231df2864843cc02
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-18 19:22:24 -0600

    Test: wrote test 2

--- spec/iteration2_spec.rb
+++ spec/iteration2_spec.rb
@@ -13,7 +13,32 @@ RSpec.describe Board do
 
     end
 
+#Test 2
+    it 'board has 16 cells' do
+       
+        board = Board.new
 
+        board.cells("A1")
+        board.cells("A2")
+        board.cells("A3")
+        board.cells("A4")
+        board.cells("B1")
+        board.cells("B2")
+        board.cells("B3")
+        board.cells("B4")
+        board.cells("C1")
+        board.cells("C2")
+        board.cells("C3")
+        board.cells("C4")
+        board.cells("D1")
+        board.cells("D2")
+        board.cells("D3")
+        board.cells("D4")
+
+
+        expect(board.cells)to eq({"A1" => cellobject, ...})
+
+    end
 
 
 end
