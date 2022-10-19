d30aabcb63176ba890f6791a194ebf8649161d6d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-18 19:27:12 -0600

    Test: Wrote test 3

--- spec/iteration2_spec.rb
+++ spec/iteration2_spec.rb
@@ -40,5 +40,44 @@ RSpec.describe Board do
 
     end
 
+#Validate Coordinates
+
+#Test 3
+    it 'Coordinates fall within the 16 possibilities' do
+
+        board = Board.new
+
+        board.cells("A1")
+        board.cells("A2")
+        board.cells("A3")
+        board.cells("A4")
+        board.cells("B1")
+        board.cells("B2")
+        board.cells("B3")
+        board.cells("B4")
+        board.cells("C1")
+        board.cells("C2")
+        board.cells("C3")
+        board.cells("C4")
+        board.cells("D1")
+        board.cells("D2")
+        board.cells("D3")
+        board.cells("D4")
+
+        board.valid_coordinate?("A1")
+        board.valid_coordinate?("D4")
+        board.valid_coordinate?("A5")
+        board.valid_coordinate?("E1")
+        board.valid_coordinate?("A22")
+
+        expect(board.valid_coordinate?("A1")).to be true
+        expect(board.valid_coordinate?("D4")).to be true
+        expect(board.valid_coordinate?("A5")).to be false
+        expect(board.valid_coordinate?("E1")).to be false
+        expect(board.valid_coordinate?("A22")).to be false
+
+    end
+
+
 
 end
