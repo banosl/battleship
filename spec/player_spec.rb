585e95f6999bc9b22a50c5ce683aff7e21e49a81
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-24 14:58:47 -0600

    Test: wrote test for #welcome

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,55 +3,60 @@ require './lib/cell'
 require './lib/ship'
 require './lib/player'
 require RSpec.describe Player do
-
+#Ryan
   describe '#initialize' do
     it 'does stuff' do
 
     end
   end
 
+  #Leo
   describe '#welcome' do
-    it 'does stuff' do
-
+    it 'prints the welcome message for the player' do
+      puts welcome
     end
   end
 
+  #leo
   describe '#get_shot' do
     it 'does stuff' do
 
     end
   end
 
-  describe '#place_ships' do
-    it 'does stuff' do
+ 
+  # describe '#place_ships' do
+  #   it 'does stuff' do
 
-    end
-  end
+  #   end
+  # end
 
+  #Leo
   describe '#random_place_ships' do
     it 'does stuff' do
 
     end
   end
 
-  describe '#take_shot' do
-    it 'does stuff' do
+  # describe '#take_shot' do
+  #   it 'does stuff' do
 
     end
   end
 
+  #Leo
   describe '#random_take_shot' do
     it 'does stuff' do
 
     end
   end
-
+#Ryan
   describe '#all_ships_sunk?' do
     it 'does stuff' do
 
     end
   end
-
+#Ryan
   describe '#coordinates_unfired_upon' do
     it 'does stuff' do
 
