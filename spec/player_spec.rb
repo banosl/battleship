5c550a2c44f5b580392bc281fcd3641bc483ba83
Ryan Canton <ryancanton999@gmail.com>
2022-10-24 14:37:03 -0600

    Adds empty describe blocks for player_spec file

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,6 +10,57 @@ require RSpec.describe Player do
     end
   end
 
+  describe '#welcome' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#get_shot' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#place_ships' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#random_place_ships' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#take_shot' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#random_take_shot' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#all_ships_sunk?' do
+    it 'does stuff' do
+
+    end
+  end
+
+  describe '#coordinates_unfired_upon' do
+    it 'does stuff' do
+
+    end
+    it 'correctly returns coordinate' do
+
+    end
+  end
+
 
 
 end
